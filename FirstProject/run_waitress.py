8aaeeb52439aaf047675574f9967c1d230302c23
DNTH\suthinan <keang_ll@hotmail.com>
2024-05-21 10:01:05 +0700

    Reset commit

---
+++ FirstProject/run_waitress.py
@@ -0,0 +1,6 @@
+from waitress import serve  # นำเข้า serve function จาก waitress
+from FirstProject.wsgi import application  # นำเข้า WSGI application จากโปรเจกต์ Django ของคุณ
+
+if __name__ == '__main__':
+    # ใช้ serve function เพื่อรันแอปพลิเคชันบนเซิร์ฟเวอร์ Waitress
+    serve(application, host='0.0.0.0', port=8000)
